48a16ed71c76b9f21dc7c207905f94b8e1b5be52
imranshuvo <imrankhanshuvo@gmail.com>
2022-08-01 14:13:52 +0600

    Improved product removal from checkout page

--- 9amdev-woo-checkout-cart.php
+++ 9amdev-woo-checkout-cart.php
@@ -57,9 +57,18 @@ function nineamdev_add_remove_and_quantity_field($product_name, $cart_item, $car
     //Get the product/variation id first
     $_product = apply_filters( 'woocommerce_cart_item_product', $cart_item['data'], $cart_item, $cart_item_key );
 
-    $html = '<a href="#" style="color: red;margin-right: 10px;">X</a>';
+    $html = apply_filters( // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+                        'woocommerce_cart_item_remove_link',
+                        sprintf(
+                            '<a href="%s" class="remove" aria-label="%s" data-product_id="%s" data-product_sku="%s">&times;</a>',
+                            esc_url( wc_get_cart_remove_url( $cart_item_key ) ),
+                            esc_html__( 'Remove this item', 'woocommerce' ),
+                            esc_attr( $_product->get_id() ),
+                            esc_attr( $_product->get_sku() )
+                        ),
+                        $cart_item_key
+                    );
     $html .= $product_name;
-    $html .= '<form></form>';
-
+    $html .= '';
     return $html;
 }
